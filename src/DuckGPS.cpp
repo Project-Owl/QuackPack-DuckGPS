88b5c867d3e5b4caeb919467019e509b0765889e
Brenton Poke <brentonpoke@outlook.com>
2026-04-27 00:27:23 -0400

    streamline log messages
    Signed-off-by: Brenton Poke <brentonpoke@outlook.com>

--- src/DuckGPS.cpp
+++ src/DuckGPS.cpp
@@ -104,7 +104,7 @@ UBXSendStatus DuckGPS::setBaudrate(uint32_t baudrate) {
     GPSSerial.end();
     if (status == UBX_SEND_SUCCESS) {
         GPSSerial.begin(baudrate, SERIAL_8N1);
-        logdbg_ln(std::string("Set GPS baudrate to: " + std::to_string(baudrate)).c_str());
+        logdbg_ln(std::string("Set GPS baudrate to: %s",std::to_string(baudrate)).c_str());
     } else {
         logdbg_ln("Failed to set GPS baudrate");
     }
@@ -153,13 +153,13 @@ std::time_t DuckGPS::epoch(){
 void DuckGPS::printData(){
     // Printing the GPS data
     logdbg_ln("--- GPS ---");
-    logdbg_ln(std::string("Latitude  : "+std::to_string(lat())).c_str());
-    logdbg_ln(std::string("Longitude : "+std::to_string(lng())).c_str());
-    logdbg_ln(std::string("Altitude : "+std::to_string(altitude(AltitudeUnit::meter))).append("M").c_str());
-    logdbg_ln(std::string("Satellites : "+std::to_string(satellites())).c_str());
-    logdbg_ln(std::string("Raw Date : "+std::to_string(gps.date.value())).c_str());
-    logdbg_ln(std::string("Epoch : "+std::to_string(epoch())).c_str());
-    logdbg_ln(std::string("Raw Date : "+std::to_string(speed(SpeedUnit::kmph))).append("KPH").c_str());
+    logdbg_ln(std::string("Latitude  : %s",std::to_string(lat())).c_str());
+    logdbg_ln(std::string("Longitude : %s",std::to_string(lng())).c_str());
+    logdbg_ln(std::string("Altitude : %s M",std::to_string(altitude(AltitudeUnit::meter))).c_str());
+    logdbg_ln(std::string("Satellites : %s",std::to_string(satellites())).c_str());
+    logdbg_ln(std::string("Raw Date : %s",std::to_string(gps.date.value())).c_str());
+    logdbg_ln(std::string("Epoch : %s",std::to_string(epoch())).c_str());
+    logdbg_ln(std::string("Raw Date : %s KPH",std::to_string(speed(SpeedUnit::kmph))).c_str());
     logdbg_ln("**********************");
 }
 
